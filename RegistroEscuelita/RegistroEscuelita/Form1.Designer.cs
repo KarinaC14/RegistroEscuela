e554acb2497af29fc007b04cc4dddfe1ce1a9322
KarinaC14 <140867412+KarinaC14@users.noreply.github.com>
2023-07-31 00:01:35 -0500

    Add files via upload

---
+++ RegistroEscuelita/RegistroEscuelita/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace RegistroEscuelita
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblEscuelita = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEscuelita
+            // 
+            this.lblEscuelita.AutoSize = true;
+            this.lblEscuelita.Location = new System.Drawing.Point(32, 21);
+            this.lblEscuelita.Name = "lblEscuelita";
+            this.lblEscuelita.Size = new System.Drawing.Size(69, 13);
+            this.lblEscuelita.TabIndex = 0;
+            this.lblEscuelita.Text = "ESCUELITA ";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(46, 270);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(56, 13);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "USUARIO";
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Location = new System.Drawing.Point(46, 309);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(41, 13);
+            this.lblClave.TabIndex = 2;
+            this.lblClave.Text = "CLAVE";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(159, 262);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(123, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(159, 302);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(123, 20);
+            this.txtClave.TabIndex = 4;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(138, 328);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "ACEPTAR";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ABC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(342, 376);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblEscuelita);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEscuelita;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RegistroEscuelita/RegistroEscuelita/RegistroNuevo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RegistroEscuelita
+{
+    public partial class RegistroNuevo : Form
+    {
+        public Escuelas escuelas = new Escuelas();
+        public RegistroNuevo()
+        {
+
+            InitializeComponent();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Escuela escuela = new Escuela();
+            escuela.NombresB = txtNombresB.Text;
+            escuela.ApellidosB = txtApellidosB.Text;
+            escuela.CedulaB = Convert.ToInt32(mtxtCedulaN.Text);
+            escuela.Genero = cmbGenero.Text;
+            escuela.Edad = Convert.ToInt32(mtxtEdad.Text);
+            escuela.NombresP = txtNombreP.Text;
+            escuela.Celular = Convert.ToInt32(mtxtCelularP.Text);
+            escuela.CedulaP = Convert.ToInt32(mtxtCedula.Text);
+            escuela.Curso = cmbGrado.Text;
+            escuelas.AgregarNinos(escuela);
+            MessageBox.Show("Sus datos fueron guardados con exito");
+            dgvDatos.DataSource = escuelas.ImprimirNinos();
+            Limpiar();
+           
+        }
+        private void Limpiar()
+        {
+            txtNombresB.Text = " ";
+            txtApellidosB.Text = " ";
+            mtxtCedulaN.Text = " ";
+            cmbGenero.Text = " ";
+            mtxtEdad.Text = " ";
+            txtNombreP.Text = " ";
+            mtxtCelularP.Text = " ";
+            mtxtCedula.Text = " ";
+            mtxtCelularP.Text = " ";
+            cmbGrado.Text = " ";
+
+        }
+        
+    }
+}
